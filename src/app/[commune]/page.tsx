4de405f0141cb28a446d78e906c48f9792d44516
Nico <ngomez@hey.com>
2024-08-25 07:43:20 -0400

    feat(report): add weather info

--- src/app/[commune]/page.tsx
+++ src/app/[commune]/page.tsx
@@ -1,5 +1,7 @@
 import { generateReport, recommendationsForTheCommunity } from "@/ai/report";
 import Markdown from 'react-markdown'
+import { getPrecipitationForecast } from "@/api/weather"
+
 
 type Parameters = {
   params: {
@@ -10,6 +12,8 @@ type Parameters = {
 export default async function Report({ params }: Parameters) {
   const reportMarkdown = await generateReport(params.commune);
   const communityRecommendations = await recommendationsForTheCommunity(params.commune, reportMarkdown);
+  const precipitationForecast = await getPrecipitationForecast(params.commune);
+  const precipitation = precipitationForecast.daily.precipitation_sum.reduce((a, b) => a + b, 0);
 
   return (
     <>
@@ -17,6 +21,10 @@ export default async function Report({ params }: Parameters) {
       <Markdown>{reportMarkdown}</Markdown>
       <h2>Recomendaciones para los vecinos</h2>
       <Markdown>{communityRecommendations}</Markdown>
+      <h2>¿Llueve o no llueve?</h2>
+      <p>
+        {precipitation > 0 ? `Sí, va a llover (${precipitation}mm)` : "No, no va a llover"}
+      </p>
     </>
   )
 }
